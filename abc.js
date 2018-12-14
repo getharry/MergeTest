ff33685f491eacef8e7696cfbe06ed535581e859
harry <haoranharry@sina.com>
2018-12-14 17:55:49 +0800

    init

---
+++ abc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/theme'
+        ]
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  },
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
+  productionSourceMap: false
+}
