e46fc327fcb98a41c1db72726571bb5d69b88b05
harry <haoranharry@sina.com>
2019-01-29 11:11:26 +0800

    test secondly

--- abc.js
+++ abc.js
@@ -12,7 +12,8 @@ module.exports = {
   abc: {
 
       // baseUrl: process.env.NODE_ENV === 'production3'
-      add: 'today is 2019.1.29'
+      add: 'today is 2019.1.29',
+      add2: 'go twice'
   },
 
 ----
